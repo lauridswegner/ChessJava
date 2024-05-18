c721b3dd3c81633d3b3c892c4b906aa015173dbe
Laurids Wegner <laurids.wegner@nordakademie.de>
2024-05-18 21:15:54 +0200

    added Board

--- src/main/Board.java
+++ src/main/Board.java
@@ -1,2 +1,26 @@
-package main;public class Board {
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JPanel {
+
+    public int tileSize = 85;
+
+    int cols = 8;
+    int rows = 8;
+    public Board() {
+        this.setPreferredSize(new Dimension(cols * tileSize, rows * tileSize));
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        for (int r = 0; r < rows; r++)
+            for (int c = 0; c < cols; c++) {
+                g2d.setColor((c+r)%2==0 ? new Color(7, 47, 124): new Color(93, 114, 176));
+                g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
+            }
+    }
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,20 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.getContentPane().setBackground(Color.black);
+        frame.setLayout(new GridBagLayout());
+        frame.setMinimumSize(new Dimension(1000, 1000));
+        frame.setLocationRelativeTo(null);
+
+        Board board = new Board();
+        frame.add(board);
+
+        frame.setVisible(true);
     }
 }
