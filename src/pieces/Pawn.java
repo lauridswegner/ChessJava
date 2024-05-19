2c1e3d25951fff3062996e08a4a1d6faa85575b3
Laurids Wegner <laurids.wegner@nordakademie.de>
2024-05-19 21:21:15 +0200

    added EnPassant-Movement and Queen-Promotion

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -42,6 +42,16 @@ public class Pawn extends Piece {
             return true;
         }
 
+        // en passant left
+        if (board.getTileNum(col, row) == board.enPassantTile && col == this.col - 1 && row == this.row - colorIndex && board.getPiece(col, row + colorIndex) != null) {
+            return true;
+        }
+
+        // en passant right
+        if (board.getTileNum(col, row) == board.enPassantTile && col == this.col + 1 && row == this.row - colorIndex && board.getPiece(col, row + colorIndex) != null) {
+            return true;
+        }
+
         return false;
 
     }
